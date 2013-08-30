e7dce15cabbb4809396e245e3db75201ed779627
pokornyr <robin.pokorny@lmc.eu>
2013-08-30 14:02:55 +0200

    Configure tests, refactor code

--- tasks/humans_txt.js
+++ tasks/humans_txt.js
@@ -13,18 +13,41 @@ module.exports = function( grunt ) {
     grunt.registerMultiTask( 'humans_txt', 'Generate information about people behind the website', function( ) {
 
         var options = this.options( {
-            commentStyle: 'c',
-            tab: '\t',
-            intro: 'The humans responsible & colophon'
-        } ),
-        formatKey = function ( str ) {
+                includeUpdateIn: 'site',
+                commentStyle: 'c',
+                tab: '\t',
+                intro: 'The humans responsible & colophon'
+            } ),
+            dest;
+
+        if ( this.files.length > 1 ) {
+            grunt.verbose.warn('Destination not written because too many destinations were provided.');
+        }
+        if ( this.files.length < 1 || !this.files[0].dest) {
+            grunt.verbose.warn('Destination not written because no destination was provided.');
+        } else {
+            dest = this.files[0].dest;
+        }
+        if ( !options.content ) {
+            grunt.verbose.warn('Destination not written because no content was provided.');
+        }
+
+        var formatKey = function ( str ) {
             return str.replace( /\w\S*/g, function( txt ) {
 
                 return txt.charAt( 0 ).toUpperCase( ) + txt.substr( 1 ).toLowerCase( ).replace( '_', ' ' );
 
             } );
         },
-        writeComment;
+        writeLastUpdate = function ( ) {
+            var date = new Date( ),
+                day = date.getDate(),
+                month = date.getMonth() + 1,
+                year = date.getFullYear();
+            return "Last update: " + year + "/"	+ month + "/" + day + '\n';
+        },
+        writeComment,
+        contents = '';
 
         switch ( options.commentStyle ) {
             case 'c':
@@ -42,75 +65,58 @@ module.exports = function( grunt ) {
                     return '// ' + str + '\n';
                 };
                 break;
+            default:
+                grunt.log.error('Unknown comment style value.');
         }
 
+        // Start creation of CACHE MANIFEST
+        if ( options.intro ) {
+          contents += writeComment( options.intro );
+        }
 
-        this.files.forEach( function( f ) {
+        contents += writeComment( 'humanstxt.org' );
 
-            var files,
-                date = ( options.date ) ? options.date : new Date( ),
-                contents = '';
 
-            // Set save location
-            if ( !f.dest ) {
-                f.dest = options.fileName;
+        for ( var section in options.content ) {
+            contents += '\n';
+
+            contents += writeComment( section.toUpperCase() );
+
+            // Optional update ( can be overriden with option value )
+            if ( options.includeUpdateIn &&
+                  typeof options.includeUpdateIn === 'string' &&
+                  section.toLowerCase() === options.includeUpdateIn.toLowerCase() ) {
+                contents += options.tab + writeLastUpdate();
             }
 
-            // Start creation of CACHE MANIFEST
-            if ( options.intro ) {
-              contents += writeComment( options.intro );
+            var sectionDetails = options.content[ section ];
+
+            if ( typeof sectionDetails === 'string' ) {
+                sectionDetails = [ ].concat( sectionDetails );
             }
 
-            contents += writeComment( 'humanstxt.org' );
+            for ( var i = 0; i < sectionDetails.length; i++ ) {
 
-            // Optional TimeStamp ( can be overriden with option value )
-            if ( options.content ) {
+                if ( typeof sectionDetails[ i ] === 'string' ) {
+                    contents += options.tab + sectionDetails[ i ] + '\n';
+                } else {
 
-                for ( var section in options.content ) {
-
-                    contents += '\n';
-
-                    contents += writeComment( section.toUpperCase() );
-
-                    if (section.toLowerCase() === 'site') {
-                      contents += options.tab + "Last update: " + date.getDate() + "/"	+ ( date.getMonth()	+ 1 ) + "/"	+ date.getFullYear() + '\n';
+                    for ( var keyTech in sectionDetails[ i ] ) {
+                        var objTech = sectionDetails[ i ][ keyTech ];
+                        contents += options.tab + formatKey(keyTech) + ": " + objTech + '\n';
                     }
 
-                    var sectionDetails = options.content[ section ];
-
-                    if ( typeof sectionDetails === 'string' ) {
-                        sectionDetails = [ ].concat( sectionDetails );
+                    if (i !== sectionDetails.length - 1) {
+                      contents += '\n';
                     }
-
-                    for ( var i = 0; i < sectionDetails.length; i++ ) {
-
-                        if ( typeof sectionDetails[ i ] === 'string' ) {
-                            contents += options.tab + sectionDetails[ i ] + '\n';
-                        } else {
-
-                            for ( var keyTech in sectionDetails[ i ] ) {
-
-                                var objTech = sectionDetails[ i ][ keyTech ];
-
-                                contents += options.tab + formatKey(keyTech) + ": " + objTech + '\n';
-
-                            }
-
-                            if (i !== sectionDetails.length - 1) {
-                                contents += '\n';
-                            }
-                        }
-
-                    }
-
                 }
             }
+        }
 
-            grunt.file.write( f.dest, contents );
+        grunt.file.write( dest, contents );
 
-            grunt.log.writeln( 'File "' + f.dest + '" created.' );
+        grunt.log.writeln( 'File "' + dest + '" created.' );
 
-        } );
 
     } );
 
